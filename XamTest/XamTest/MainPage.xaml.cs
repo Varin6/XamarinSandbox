a35b09990a62b14a02eb744fb132fee281c42176
Varin6 <varinator@gmail.com>
2018-10-17 23:47:35 +0100

    Initial

---
+++ XamTest/XamTest/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+          
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
+            {
+                conn.CreateTable<Book>();
+
+                var books = conn.Table<Book>().ToList();
+                booksListView.ItemsSource = books;
+                
+                //booksListView.ItemSelected
+                
+            }
+
+            
+        }
+
+        private void ToolbarItem_Activated(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewXamTestPage());
+            
+        }
+
+        public void OnMore(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            DisplayAlert("MORE", "Nothing here yet...", "OK");
+        }
+
+        public void OnDelete(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+
+
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
+            {
+
+                var item = mi.CommandParameter as object;
+                conn.Delete(item);
+
+                var books = conn.Table<Book>().ToList();
+                booksListView.ItemsSource = books;
+
+            
+                DisplayAlert("DELETED", "Bobe has been deleted!", "OK");
+
+            }
+
+
+
+            
+        }
+    }
+}
---
+++ XamTest/XamTest/NewXamTestPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamTest
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewXamTestPage : ContentPage
+	{
+		public NewXamTestPage ()
+		{
+            InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Book book = new Book()
+            {
+                Name = nameEntry.Text,
+                Author = authorEntry.Text
+            };
+
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
+            {
+                conn.CreateTable<Book>();
+                var numberOfROws = conn.Insert(book);
+                Navigation.PopAsync(animated:true);
+                
+                if (numberOfROws > 0)
+                    DisplayAlert("Success", "Bobe was successfully inserted", "Fortuitous!");
+                
+                else
+                    DisplayAlert("Failure", "Bobe failed insertion", "Unfortuitous...");
+            }
+            
+
+            //DisplayAlert("Success", "You've managed to click the button!", "Fortuitous!");
+        }
+    }
+}
